2067bbfad32a1418631c61b906f9da5a504d6637
Ulan Yerikov <erikov25.05@gmail.com>
2021-11-01 21:53:47 +0600

    project3

--- practice2/src/main/java/com/example/practice2/DAO/Dao.java
+++
@@ -1,5 +0,0 @@
-package com.example.practice2.DAO;
-
-public interface Dao {
-
-}
--- practice2/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-create table Staff(Staff_id int Primary key,
-                   first_name varchar(50),
-                   last_name varchar(50),
-                   phone_number numeric,
-                   Age int,
-                   stuff_type varchar(50),
-                   salary numeric);
